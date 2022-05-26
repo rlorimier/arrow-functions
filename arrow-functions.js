f158dc0346c338ed52f3ca604f79b39ec50ac8f6
rlorimier <rere.rnt@gmail.com>
2022-05-26 22:58:15 +0100

    Update arrow-functions.js

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers (a, b) {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => {
+    return (a + b);
+}
+let sum2 = addTwoNumbers(3, 5);
+console.log(sum2);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a, b) => a + b;
+let sum3 = addTwoNumbers3(3, 5);
+console.log(sum3);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello World");
+
+const saySomething2 = () => console.log("Hello!");
+saySomething2();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiple string
+    </p>`
+)
+console.log(returnMultipleLines());
